b277cb3b785629aaaedc126ba5421fbce5e0b870
Brad Bettes <bradbettes@gmail.com>
2025-04-04 20:42:10 -0500

    Cleaned up syntax errors; committed after compile & flash
    Signed-off-by: Brad Bettes <bradbettes@gmail.com>

--- keyboards/planck/rev7/keymaps/blackfeather/keymap.c
+++ keyboards/planck/rev7/keymaps/blackfeather/keymap.c
@@ -49,11 +49,13 @@ enum planck_keycodes {
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define SPACE_FN LT(_SPACE_FN, KC_SPC)
-#define FN MO(_FN)
 #define SFT_CPS (LSFT_T(KC_CAPS))
 #define SHF_ENT (LSFT_T(KC_ENT))
 
 #define QWERTY PDF(_QWERTY)
+
+// #define FN MO(_FN)
+
 // #define COLEMAK PDF(_COLEMAK)
 // #define DVORAK PDF(_DVORAK)
 
@@ -68,14 +70,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * |------+------+------+------+------+------+------+------+------+------+------+------|
   * | Sh/CL|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Sh/Ent|
   * |------+------+------+------+------+------+------+------+------+------+------+------|
-  * |  FN  | Ctrl | Alt  | GUI  |Lower |    Space    |Raise |   /  | Left | Down |Right |
+  * |      | Ctrl | Alt  | GUI  |Lower |    Space    |Raise |   /  | Left | Down |Right |
   * `-----------------------------------------------------------------------------------'
  */
  [_QWERTY] = LAYOUT_planck_mit(
     QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,     KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     SFT_CPS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,     KC_M,    KC_COMM, KC_DOT,  KC_UP,   SHF_ENT,
-    FN,      KC_LCTL, KC_LALT, KC_LGUI, LOWER,      SPACE_FN,       RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
+    XXXXXXX, KC_LCTL, KC_LALT, KC_LGUI, LOWER,      SPACE_FN,       RAISE,   KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Lower
@@ -129,7 +131,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     QK_BOOT, KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, 
     DB_TOGG, KC_F11,  KC_F12,  XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, 
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, MS_BTN1, MS_UP,   MS_BTN2, 
-    XXXXXXX, UG_SPDD, UG_SPDU, XXXXXXX,  KC_VOLD,      XXXXXXX,     KC_VOLU, XXXXXXX, MS_LEFT, MS_DOWN, MS_RGHT, 
+    XXXXXXX, UG_SPDD, UG_SPDU, XXXXXXX,  KC_VOLD,      XXXXXXX,     KC_VOLU, XXXXXXX, MS_LEFT, MS_DOWN, MS_RGHT 
 )
 
 /*  <--------------------------------------- FN ---------------------------------------->
