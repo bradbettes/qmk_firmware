7023697c7bcb47328fec0164d33b4461e3ffffbf
Brad Bettes <bradbettes@gmail.com>
2025-05-21 03:08:13 -0500

    Added Imperial March. Changed default tempo from 120 to 30 to get timing right on Imperial March

--- keyboards/planck/keymaps/Brad/config.h
+++ keyboards/planck/keymaps/Brad/config.h
@@ -17,7 +17,7 @@
 #pragma once
 
 #ifdef AUDIO_ENABLE
-#    define STARTUP_SONG SONG(ODE_TO_JOY)
+#    define STARTUP_SONG SONG(IMPERIAL_MARCH)
 // #define STARTUP_SONG SONG(NO_SOUND)
 
 #    define DEFAULT_LAYER_SONGS \
